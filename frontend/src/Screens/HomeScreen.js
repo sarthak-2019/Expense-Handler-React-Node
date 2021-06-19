3239b0427a39e1ec82a1b6ff3271ff900b8d318f
Sarthak Shukla <sarthak2019fractal@gmail.com>
2021-06-19 20:41:53 +0530

    Added react-particles-js

--- frontend/src/Screens/HomeScreen.js
+++ frontend/src/Screens/HomeScreen.js
@@ -2,6 +2,7 @@ import React, { useState, useContext } from "react";
 import "./HomeScreen.css";
 import AuthContext from "../context/AuthContext";
 import { useHistory } from "react-router-dom";
+import Particle from "../components/Particles/Particle";
 import axios from "axios";
 
 const HomeScreen = () => {
@@ -89,84 +90,89 @@ const HomeScreen = () => {
     // setUserPassword("");
   }
   return (
-    <div className="main-body">
-      <h1>Welcome To Expense Handler App</h1>
-      <div className="container" id="container">
-        <div className="form-container sign-up-container">
-          <form onSubmit={submitHandler_SignUp}>
-            <h1>Create Account</h1>
-            <span>or use your email for registration</span>
-            <input
-              type="text"
-              placeholder="Name"
-              value={enteredName}
-              onChange={nameChangeHandler}
-              required
-            />
-            <input
-              type="email"
-              placeholder="Email"
-              value={enteredEmail}
-              onChange={emailChangeHandler}
-              required
-            />
-            <input
-              type="password"
-              minLength="4"
-              placeholder="Password"
-              value={enteredPassword}
-              onChange={passwordChangeHandler}
-              required
-            />
-            <input
-              type="password"
-              minLength="4"
-              placeholder="Confirm Password"
-              value={enteredConfirmPassword}
-              onChange={confirmPasswordChangeHandler}
-              required
-            />
-            <button>Sign Up</button>
-          </form>
-        </div>
-        <div className="form-container sign-in-container">
-          <form onSubmit={submitHandler_SignIn}>
-            <h1>Sign in</h1>
-            <span>or use your account</span>
-            <input
-              type="email"
-              placeholder="Email"
-              value={userEmail}
-              onChange={userNameChangeHandler}
-            />
-            <input
-              type="password"
-              placeholder="Password"
-              value={userPassword}
-              onChange={userPasswordChangeHandler}
-            />
-            <button>Sign In</button>
-          </form>
-        </div>
-        <div className="overlay-container">
-          <div className="overlay">
-            <div className="overlay-panel overlay-left">
-              <h1>Welcome Back!</h1>
-              <p>To keep connected please login with your personal info</p>
-              <button className="ghost" id="signIn" onClick={buttonHandler1}>
-                Sign In
-              </button>
-            </div>
-            <div className="overlay-panel overlay-right">
-              <h1>Hello !</h1>
-              <p>
-                Sign Up for start using the service. Hope you will like the
-                experience.Visit my Github repository for more interesting
-                projects.
-              </p>
-              <button className="ghost" id="signUp" onClick={buttonHandler2}>
-                Sign Up
-              </button>
+    <div>
+      <Particle />
+      <div className="main-body">
+        <h1>Welcome To Expense Handler App</h1>
+        <div className="container" id="container">
+          <div className="form-container sign-up-container">
+            <form onSubmit={submitHandler_SignUp}>
+              <h1>Create Account</h1>
+              <span>or use your email for registration</span>
+              <input
+                type="text"
+                placeholder="Name"
+                value={enteredName}
+                onChange={nameChangeHandler}
+                required
+              />
+              <input
+                type="email"
+                placeholder="Email"
+                value={enteredEmail}
+                onChange={emailChangeHandler}
+                required
+              />
+              <input
+                type="password"
+                minLength="4"
+                placeholder="Password"
+                value={enteredPassword}
+                onChange={passwordChangeHandler}
+                required
+              />
+              <input
+                type="password"
+                minLength="4"
+                placeholder="Confirm Password"
+                value={enteredConfirmPassword}
+                onChange={confirmPasswordChangeHandler}
+                required
+              />
+              <button>Sign Up</button>
+            </form>
+          </div>
+          <div className="form-container sign-in-container">
+            <form onSubmit={submitHandler_SignIn}>
+              <h1>Sign in</h1>
+              <span>or use your account</span>
+              <input
+                type="email"
+                placeholder="Email"
+                value={userEmail}
+                onChange={userNameChangeHandler}
+                required
+              />
+              <input
+                type="password"
+                placeholder="Password"
+                value={userPassword}
+                onChange={userPasswordChangeHandler}
+                required
+              />
+              <button>Sign In</button>
+            </form>
+          </div>
+          <div className="overlay-container">
+            <div className="overlay">
+              <div className="overlay-panel overlay-left">
+                <h1>Welcome Back!</h1>
+                <p>To keep connected please login with your personal info</p>
+                <button className="ghost" id="signIn" onClick={buttonHandler1}>
+                  Sign In
+                </button>
+              </div>
+              <div className="overlay-panel overlay-right">
+                <h1>Hello !</h1>
+                <p>
+                  Sign Up for start using the service. Hope you will like the
+                  experience.Visit my Github repository for more interesting
+                  projects.
+                </p>
+                <button className="ghost" id="signUp" onClick={buttonHandler2}>
+                  Sign Up
+                </button>
+              </div>
             </div>
           </div>
         </div>
